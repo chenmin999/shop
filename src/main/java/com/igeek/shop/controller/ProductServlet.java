757ba949a5fa4fb227d42ede5da5abe4c8f32673
chenmin999 <270144864@qq.com>
2021-01-26 12:03:44 +0800

    shop实现浏览商品足迹

--- src/main/java/com/igeek/shop/controller/ProductServlet.java
+++ src/main/java/com/igeek/shop/controller/ProductServlet.java
@@ -7,9 +7,13 @@ import com.igeek.shop.vo.PageVO;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -39,7 +43,7 @@ public class ProductServlet extends BasicServlet {
     }
 
 
-    //查看商品列表
+    //查看商品列表，查看浏览足迹的信息
     public void viewProductListByCidPname(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //从请求参数中获取当前页
         String page = request.getParameter("pageNow");
@@ -64,12 +68,33 @@ public class ProductServlet extends BasicServlet {
         PageVO<Product> vo = productService.viewProductsByCidPname(cid, pname, pageNow);
         request.setAttribute("vo",vo);
 
+        /**
+         * 浏览商品列表页，查看到足迹信息
+         */
+        List<Product> historyProductList = new ArrayList<>();
+        Cookie[] cookies = request.getCookies();
+        if(cookies!=null){
+            for (Cookie cookie : cookies) {
+                if(cookie!=null){
+                    if("pids".equals(cookie.getName())){
+                        String pids = cookie.getValue();  //5-10-6
+                        for (String pid : pids.split("-")) {
+                            Product product = productService.viewProductByPid(pid);
+                            historyProductList.add(product);
+                        }
+                    }
+                }
+            }
+        }
+
+        //浏览过的商品集合存储到请求属性中
+        request.setAttribute("historyProductList",historyProductList);
         //跳转页面
         request.getRequestDispatcher("product_list.jsp").forward(request,response);
     }
 
 
-    //通过商品编号查询商品信息
+    //实现商品详情页展示：通过商品编号查询商品信息
     public void viewProductByPid(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //获得请求参数
         String pid = request.getParameter("pid");
@@ -92,6 +117,46 @@ public class ProductServlet extends BasicServlet {
         //获得请求参数，搜索条件
         String pname = request.getParameter("pname");
         request.setAttribute("pname",pname);
+
+        /**
+         * 浏览商品详情页：产生足迹，将浏览过的商品信息添加至Cookie中
+         */
+        String value = pid;
+
+        //若不是第一个浏览的商品，则需要从请求中获得当前Cookie的商品信息 - pids
+        Cookie[] cookies = request.getCookies();
+        if(cookies!=null){
+            for (Cookie cookie : cookies) {
+                if(cookie!=null){
+                    if("pids".equals(cookie.getName())){
+                        String pids = cookie.getValue();  //10-5-6
+                        List<String> strings = Arrays.asList(pids.split("-"));  //[10,5,6]
+                        LinkedList<String> list = new LinkedList<>(strings);  //10,5,6
+                        //若之前浏览过此商品  10,5,6
+                        if(list.contains(pid)){
+                            //先移除
+                            list.remove(pid);  //10,6
+                        }
+                        //将最新浏览的商品id存储至集合的第一个位置
+                        list.addFirst(pid);  //5,10,6
+
+                        //再用 - 拼接回去
+                        value = "";
+                        for (int i = 0; i < list.size() && i<7 ; i++) {
+                            value += list.get(i) + "-";  //5-10-6-
+                        }
+                        value = value.substring(0,value.length()-1);  //5-10-6
+                    }
+                }
+            }
+        }
+
+        //若是第一个浏览的商品，则直接将当前商品的id编号直接存储值Cookie
+        Cookie pids = new Cookie("pids",value);
+        //设置有效期
+        pids.setMaxAge(7*24*60*60);
+        //通过响应添加至客户端
+        response.addCookie(pids);
 
         request.getRequestDispatcher("product_info.jsp").forward(request,response);
     }
--- src/main/webapp/product_list.jsp
+++ src/main/webapp/product_list.jsp
@@ -120,21 +120,26 @@
 	<!-- 分页结束 -->
 
 	<!--商品浏览记录-->
-	<div
-		style="width: 1210px; margin: 0 auto; padding: 0 9px; border: 1px solid #ddd; border-top: 2px solid #999; height: 246px;">
+	<div style="width: 1210px; margin: 0 auto; padding: 0 9px; border: 1px solid #ddd; border-top: 2px solid #999; height: 246px;">
 
 		<h4 style="width: 50%; float: left; font: 14px/30px 微软雅黑">浏览记录</h4>
-		<div style="width: 50%; float: right; text-align: right;">
+
+		<%--<div style="width: 50%; float: right; text-align: right;">
 			<a href="">more</a>
-		</div>
+		</div>--%>
+
 		<div style="clear: both;"></div>
 
 		<div style="overflow: hidden;">
 
 			<ul style="list-style: none;">
-				<li
-					style="width: 150px; height: 216; float: left; margin: 0 8px 0 0; padding: 0 18px 15px; text-align: center;"><img
-					src="products/1/cs10001.jpg" width="130px" height="130px" /></li>
+				<c:forEach items="${historyProductList}" var="product">
+					<li style="width: 150px; height: 216px; float: left; margin: 0 8px 0 0; padding: 0 18px 15px; text-align: center;">
+						<a href="${path}/product?method=viewProductByPid&pid=${product.pid}&cid=${vo.query1}&pname=${vo.query2}&pageNow=${vo.pageNow}">
+							<img src="${path}/${product.pimage}" width="130px" height="130px" />
+						</a>
+					</li>
+				</c:forEach>
 			</ul>
 
 		</div>
